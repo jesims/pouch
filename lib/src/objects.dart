3be289b3a2111634c3f6a7895ec2b7ef0e7bdc3b
André de Jager <andrethehunter@gmail.com>
2022-01-17 09:28:54 +1000

    feat: add objects.toString

--- lib/src/objects.dart
+++ lib/src/objects.dart
@@ -5,18 +5,25 @@ import 'booleans.dart';
 /// `since 0.0.1`
 T identity<T>(T x) => x;
 
-/// True if the dynamic [value] is null
+/// Returns `true` if the dynamic [value] is `null`
 ///
 /// `since 0.0.1`
 bool isNull(dynamic value) => value == null;
 
-/// True if the dynamic [value] is not null
+/// Returns `true` if the dynamic [value] is not `null`
 ///
 /// `since 0.0.1`
 bool isNotNull(dynamic value) => isFalse(isNull(value));
 
-/// Casts [x] to the specified type [T] if it is an instance of [T].
-/// Null if [x] is not an instance of [T]
+/// If [x] is an instance of [T], returns [x] cast to [T],
+/// otherwise returns `null`.
 ///
 /// `since 0.0.1`
 T? cast<T>(dynamic x) => x is T ? x : null;
+
+/// Calls `toString` on [x]
+///
+/// `since 0.0.1`
+String? toString(dynamic x) {
+  return x?.toString();
+}
