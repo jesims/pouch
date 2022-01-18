8050e49db9989d4c06e11162835f44ee09bdf5d2
Alexander Scott <xander@axrs.io>
2022-01-18 16:20:33 +1000

    Feature: containsIgnoreCase and equalsIgnoreCase

--- test/strings_test.dart
+++ test/strings_test.dart
@@ -19,4 +19,29 @@ void main() {
     expect(strings.isBlank('value'), isFalse);
     expect(strings.isBlank('\t   \t hiya \t'), isFalse);
   });
+
+  test('equalsIgnoreCase()', () {
+    expect(strings.equalsIgnoreCase('', ''), isTrue);
+    expect(strings.equalsIgnoreCase('Left ', 'left'), isFalse);
+    expect(strings.equalsIgnoreCase('Left', 'LEFT'), isTrue);
+    expect(strings.equalsIgnoreCase('Left', 'left'), isTrue);
+    expect(strings.equalsIgnoreCase('Left', null), isFalse);
+    expect(strings.equalsIgnoreCase('left', 'right'), isFalse);
+    expect(strings.equalsIgnoreCase(null, 'right'), isFalse);
+    expect(strings.equalsIgnoreCase(null, null), isTrue);
+  });
+
+  test('containsIgnoreCase()', () {
+    expect(strings.containsIgnoreCase('', ''), isTrue);
+    expect(strings.containsIgnoreCase('Left ', 'left'), isTrue);
+    expect(strings.containsIgnoreCase('Left', 'LEFT'), isTrue);
+    expect(strings.containsIgnoreCase('Left', null), isFalse);
+    expect(strings.containsIgnoreCase('left', 'right'), isFalse);
+    expect(
+      strings.containsIgnoreCase('A longer sentence with Words', 'with words'),
+      isTrue,
+    );
+    expect(strings.containsIgnoreCase(null, 'right'), isFalse);
+    expect(strings.containsIgnoreCase(null, null), isTrue);
+  });
 }
