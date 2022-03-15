19d431a084f6f08a80c06129336cde835b7f4837
André de Jager <AndreTheHunter@gmail.com>
2022-03-15 15:33:56 +1000

    doc: docstrings

--- lib/src/resource.dart
+++ lib/src/resource.dart
@@ -1,9 +1,11 @@
-///
+/// [Resource] and [ResourceSync] utils
 ///
 /// `since 0.8.0`
 //TODO remove once https://github.com/dart-lang/language/issues/2051 is done
 class ResourceUtils {
-  ///
+  /// Finally [Resource.dispose]s a [resource] after passing it to [fn].
+  /// Returns the result of [fn].
+  /// See also [Resource.using]
   ///
   /// `since 0.8.0`
   static Future<R> using<R, T extends Resource>(
@@ -17,7 +19,7 @@ class ResourceUtils {
     }
   }
 
-  ///
+  /// Synchronous version of [ResourceUtils.using]
   ///
   /// `since 0.8.0`
   static R usingSync<R, T extends Resource>(
@@ -32,17 +34,17 @@ class ResourceUtils {
   }
 }
 
-///
+/// Synchronous version of [Resource]
 ///
 /// `since 0.8.0`
 //TODO remove once https://github.com/dart-lang/sdk/issues/43490 is done
 mixin ResourceSync {
-  ///
+  /// Synchronous version of [Resource.dispose].
   ///
   /// `since 0.8.0`
   void disposeSync();
 
-  ///
+  /// Calls [ResourceUtils.usingSync] on `this`.
   ///
   /// `since 0.8.0`
   R using<R, S extends Resource>(R Function(S) fn) {
@@ -50,17 +52,18 @@ mixin ResourceSync {
   }
 }
 
-///
+/// A resource that needs to be [dispose]d (cleaned up)
 ///
 /// `since 0.8.0`
 //TODO remove once https://github.com/dart-lang/sdk/issues/43490 is done
 mixin Resource {
-  ///
+  /// Stops, cancels, and cleans up the resource.
+  /// Should be called in a `finally` block.
   ///
   /// `since 0.8.0`
   Future<void> dispose();
 
-  ///
+  /// Calls [ResourceUtils.using] on `this`.
   ///
   /// `since 0.8.0`
   Future<R> using<R, S extends Resource>(Future<R> Function(S) fn) async {
