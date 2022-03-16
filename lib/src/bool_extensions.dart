3809fda15dc87d13dc0a6c1362f3e169e5277d3c
André de Jager <AndreTheHunter@gmail.com>
2022-03-16 12:41:57 +1000

    feat: changes

--- lib/src/bool_extensions.dart
+++ lib/src/bool_extensions.dart
@@ -6,9 +6,4 @@ extension BoolExtensions on bool {
   ///
   /// `since 0.7.0`
   bool get isFalse => this == false;
-
-  /// Returns `true` when [this] is `true`
-  ///
-  /// `since 0.7.0`
-  bool get isTrue => this == true;
 }
