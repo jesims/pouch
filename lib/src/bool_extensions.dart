ac92ec009272f43310ea7be4857e14fd378047cd
André de Jager <AndreTheHunter@gmail.com>
2022-03-16 12:56:48 +1000

    feat: NullableBoolExtensions

--- lib/src/bool_extensions.dart
+++ lib/src/bool_extensions.dart
@@ -7,3 +7,18 @@ extension BoolExtensions on bool {
   /// `since 0.7.0`
   bool get isFalse => this == false;
 }
+
+/// Extensions to [bool]?
+///
+/// `since 1.0.0`
+extension NullableBoolExtensions on bool? {
+  /// Returns `true` when [this] is `true`
+  ///
+  /// `since 1.0.0`
+  bool get isTrue => this == true;
+
+  /// Returns `true` when [this] is `false`
+  ///
+  /// `since 1.0.0`
+  bool get isFalse => this == false;
+}
