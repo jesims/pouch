af27908e8f0a6ac0aadd6640a9c4161ab447c98e
Alexander Scott <xander@axrs.io>
2022-04-26 14:51:43 +1000

    Doc: Add since attributes

--- lib/src/models/date.dart
+++ lib/src/models/date.dart
@@ -2,18 +2,23 @@ import 'package:equatable/equatable.dart';
 import 'package:intl/intl.dart';
 
 /// A Basic representation of a Date
+/// `since 1.4.0`
 class Date extends Equatable {
   /// The calendar year
+  /// `since 1.4.0`
   final int year;
 
   /// The month of the [year], 1-indexed.
+  /// `since 1.4.0`
   final int month;
 
   /// The day of the [month], 1-indexed.
+  /// `since 1.4.0`
   final int day;
 
   /// Returns `true` if the provided [year], [month], and [day] values can be
   /// parsed into a valid [Date] object.
+  /// `since 1.4.0`
   static bool isValidDate(int year, int month, int day) {
     try {
       var yearStr = year.toString().padLeft(4, '0');
@@ -27,6 +32,7 @@ class Date extends Equatable {
   }
 
   /// Create a [Date] from the provided values.
+  /// `since 1.4.0`
   Date({
     required this.year,
     this.month = 1,
--- lib/src/models/time.dart
+++ lib/src/models/time.dart
@@ -1,17 +1,22 @@
 import 'package:equatable/equatable.dart';
 
 /// A Basic representation of Time in 24 hour format
+/// `since 1.4.0`
 class Time extends Equatable {
   /// The total number of hours into the day (0-23).
+  /// `since 1.4.0`
   final int hour;
 
   /// The total number of minutes past the [hour] (0-59).
+  /// `since 1.4.0`
   final int minute;
 
   /// The total number of seconds past the [minute] (0-59).
+  /// `since 1.4.0`
   final int second;
 
   /// Create a [Time] from the provided values.
+  /// `since 1.4.0`
   const Time({
     this.hour = 0,
     this.minute = 0,
