d5eafaf1f1c164caceaafd0a2acf9d234a48435d
Alexander Scott <xander@axrs.io>
2022-08-08 18:22:36 +1000

    Feature: Waiter and PollingWaiter

--- lib/src/wait.dart
+++ lib/src/wait.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
-import 'future_extensions.dart';
+import '../pouch.dart';
+import 'package:meta/meta.dart';
 
 /// Wait Helper Utilities
 ///
@@ -28,6 +29,55 @@ class Wait {
         throw TimeoutException('timed out waiting for $taskName');
       }
       await Future<void>.delayed(checkInterval);
+    }
+  }
+}
+
+/// Utility class that aids in waiting for a predicate to be met.
+///
+/// `since 1.7.0`
+abstract class Waiter {
+  /// Waits for the [task] to be finished (identified by [isFinished]).
+  Future<void> until(
+    String task,
+    FutureOr<bool> Function() isFinished,
+  );
+}
+
+/// A polling implementation of [Waiter]
+///
+/// `since 1.7.0`
+class PollingWaiter implements Waiter {
+  /// The Duration to wait before any predicate to become true
+  final Duration timeout;
+
+  /// The duration to wait between predicate checks
+  final Duration interval;
+
+  /// Implementation of [Clock] for time keeping
+  @visibleForTesting
+  final Clock clock;
+
+  /// Implementation of performing a wait for a specified [duration]
+  @visibleForTesting
+  Future<void> Function(Duration duration) delay = Future<void>.delayed;
+
+  /// Constructs a new instance of [PollingWaiter]
+  PollingWaiter({
+    this.timeout = const Duration(seconds: 30),
+    this.interval = const Duration(milliseconds: 100),
+    this.clock = const DartClock(),
+  });
+
+  @override
+  Future<void> until(String task, FutureOr<bool> Function() isFinished) async {
+    var timeoutEpochMs =
+        clock.millisecondsSinceEpoch() + timeout.inMilliseconds;
+    while (await isFinished().isFalse) {
+      if (timeoutEpochMs < clock.millisecondsSinceEpoch()) {
+        throw TimeoutException('Waiting for $task');
+      }
+      await delay(interval);
     }
   }
 }
